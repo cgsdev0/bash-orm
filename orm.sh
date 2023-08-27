55be3029174989ac3075c8c5fcad8429185534fd
Sarah Schulte <git@sarah.engineer>
2023-08-27 03:39:12 -0700

    well it does something

--- orm.sh
+++ orm.sh
@@ -1,1 +1,32 @@
 #!/usr/bin/env bash
+
+function dog() {
+  cat
+}
+
+function query() {
+  function _query() {
+    local QUERY
+    QUERY="$1"
+    shift
+    printf ".parameter init\n"
+    INDEX=1
+    for arg in "$@"; do
+      : "${arg//\\/\\\\\\\\}"
+      : "${_//$'\t'/    }"
+      : "${_//\"/\\\"}"
+      : "${_//\'/\\\'}"
+      : "${_//$'\n'/\\\\n}"
+      if [[ "$_" =~ "'" ]]; then
+        printf ".parameter set ?%d \"%s\"\n" $INDEX "$_"
+      else
+        printf ".parameter set ?%d \"'%s'\"\n" $INDEX "$_"
+      fi
+      ((INDEX++))
+    done
+    printf "%s\n" "$QUERY";
+  }
+  sqlite3 -separator $'\t' $ORM_DB < <(_query "$@")
+}
+
+export -f query
--- test.sh
+++ test.sh
@@ -3,4 +3,10 @@
 set -eu
 source orm.sh
 
-echo "Test complete!"
+ORM_DB=test.db
+
+query "SELECT ?1, ?2" "'param1" '"param2'
+query "SELECT ?1, ?2" "1+1" "'1+1"
+query "SELECT ?1 as hey, ?2" $'\t\t' 'hello
+
+world\a\n'
